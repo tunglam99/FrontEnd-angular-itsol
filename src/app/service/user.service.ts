e22e8a73c60a78c2527537c05f71da94437e95c8
tunglam99 <lamnthnue.edu.vn@gmail.com>
2020-07-14 10:18:38 +0700

    done

---
+++ src/app/service/user.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {Observable} from "rxjs";
+import {environment} from "../../environments/environment";
+import {User} from "../model/user";
+
+const API_URL = environment.apiUrl;
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+
+  constructor(private http: HttpClient) { }
+
+  login (user: User): Observable<User> {
+    return this.http.post<User>(API_URL + '/login', user);
+  }
+}
